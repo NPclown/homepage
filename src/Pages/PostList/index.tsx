6d1aba47b558452d499ea69ad027695f7a0e4333
npclown <nigrumspiritus@gmail.com>
2022-01-01 00:29:05 +0900

    Header 구현

--- src/Pages/PostList/index.tsx
+++ src/Pages/PostList/index.tsx
@@ -6,14 +6,15 @@ import Setting from "@components/Setting";
 import ListArticle from "@components/ListArticle";
 import GalleryArticle from "@components/GalleryArticle";
 import Pagination from "@components/Pagination";
+import './index.css';
 
 //TODO :: 
 const PostList = () => {
   return (
-    <div className="">
+    <div className="container">
         <Header></Header>
         <Title></Title>
-        <Setting></Setting>
+        <Setting></Setting> 
         <ListArticle></ListArticle>
         <GalleryArticle></GalleryArticle>
         <Pagination></Pagination>
