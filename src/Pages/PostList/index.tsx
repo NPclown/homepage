71d4128e0a185d9f3608d35110e94690587a7aa0
DongHyun, Son <nigrumspiritus@gmail.com>
2022-01-02 00:12:48 +0900

    Add/routing (#4)
    * Routing 구현
    * Header Link 구현
    * Setting 클릭 이벤트 구현
    * Pagination 클릭 이벤트 구현

--- src/Pages/PostList/index.tsx
+++ src/Pages/PostList/index.tsx
@@ -1,26 +1,32 @@
-import React, { useDebugValue } from 'react';
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import React, { useState } from 'react';
 import Title from "@components/Title";
 import Setting from "@components/Setting";
 import ListArticle from "@components/ListArticle";
 import GalleryArticle from "@components/GalleryArticle";
 import Pagination from "@components/Pagination";
 
-import Article from "@interfaces/article";
 import './index.css';
 
-//TODO :: 
+//TODO :: setting 설정
+//TODO :: 이미지 리스트 및 갤리러 형 변경
+//TODO :: 정렬 구현
 const PostList = () => {
+  const [view, setView] = useState<boolean>(true);
+  const [sort, setSort] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+  const totalPage:number = 11;
+  
+  const changeView = (viewType:boolean) => setView(viewType);
+  const changeSort = (viewSort:number) => setSort(viewSort);
+  const changePage = (currentPage:number) => setPage(currentPage);
+
   return (
     <div className="container">
-        <Header></Header>
         <Title></Title>
-        <Setting></Setting> 
-        <ListArticle></ListArticle>
-        <GalleryArticle></GalleryArticle>
-        <Pagination></Pagination>
-        <Footer></Footer>
+        <Setting changeView={changeView} changeSort={changeSort}></Setting> 
+        {console.log({sort:sort, page:page})}
+        {view ? <ListArticle></ListArticle> : <GalleryArticle></GalleryArticle>} 
+        <Pagination page={page} totalPage={totalPage} changePage={changePage}></Pagination>
     </div>
   );
 }
