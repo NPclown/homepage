a5bcd906b87b6458fb3121b4f6eca9c748742150
npclown <nigrumspiritus@gmail.com>
2022-01-01 23:47:50 +0900

    Setting 클릭 이벤트 구현

--- src/Components/Setting/index.tsx
+++ src/Components/Setting/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { BsGearFill, BsGridFill, BsListTask } from 'react-icons/bs';
 import "./index.css";
 
-const Settting = () => {
+interface settingProps{
+  changeView: (typeView:boolean) => void,
+  changeSort: (typeSort:number) => void,
+}
+//TODO :: 포스트 개수 받아오기
+const Settting = ( settingProp : settingProps) => {
   const count = 123;
     return (
       <div className="setting">
@@ -10,13 +15,13 @@ const Settting = () => {
             <span className="result-count">{count}</span>개의 포스트
           </div>
           <div className="options">
-            <BsGearFill className="setting-icon" size="30"></BsGearFill>
-            <BsGridFill className="setting-icon" size="30"></BsGridFill>
-            <BsListTask className="setting-icon" size="30"></BsListTask>
+            <BsGearFill className="setting-icon" size="30" onClick={e => alert("미구현")}></BsGearFill>
+            <BsGridFill className="setting-icon" size="30" onClick={e => settingProp.changeView(true)}></BsGridFill>
+            <BsListTask className="setting-icon" size="30" onClick={e => settingProp.changeView(false)}></BsListTask>
           </div>
-          <select className="sort">
-            <option>최신순</option>
-            <option>오래된 순</option>
+          <select className="sort" onChange={e => settingProp.changeSort(parseInt(e.target.value))}>
+            <option value={1}>최신순</option>
+            <option value={2}>오래된 순</option>
           </select>
       </div>
     );
--- src/Pages/PostList/index.tsx
+++ src/Pages/PostList/index.tsx
@@ -1,21 +1,28 @@
-import React, { useDebugValue } from 'react';
+import React, { useState } from 'react';
 import Title from "@components/Title";
 import Setting from "@components/Setting";
 import ListArticle from "@components/ListArticle";
 import GalleryArticle from "@components/GalleryArticle";
 import Pagination from "@components/Pagination";
 
-import Article from "@interfaces/article";
 import './index.css';
 
-//TODO :: 
+//TODO :: setting 설정
+//TODO :: 이미지 리스트 및 갤리러 형 변경
+//TODO :: 정렬 구현
 const PostList = () => {
+  const [view, setView] = useState<boolean>(true);
+  const [sort, setSort] = useState<number>(1);
+  
+  const changeView = (viewType:boolean) => setView(viewType);
+  const changeSort = (viewSort:number) => setSort(viewSort);
+
   return (
     <div className="container">
         <Title></Title>
-        <Setting></Setting> 
-        <ListArticle></ListArticle>
-        <GalleryArticle></GalleryArticle>
+        <Setting changeView={changeView} changeSort={changeSort}></Setting> 
+        {console.log({sort:sort})}
+        {view ? <ListArticle></ListArticle> : <GalleryArticle></GalleryArticle>} 
         <Pagination></Pagination>
     </div>
   );
