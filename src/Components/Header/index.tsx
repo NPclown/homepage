2a1d38480bb1fb60cad070c3f0b4ef8c9ec9ca1f
npclown <nigrumspiritus@gmail.com>
2022-01-03 23:30:11 +0900

    Search 컴포넌트 구현

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "@components/Search";
 import {Link} from "react-router-dom";
 import { BsSearch, BsPersonCircle } from 'react-icons/bs';
 import "./index.css";
@@ -6,6 +7,8 @@ import "./index.css";
 //TODO :: Click 이벤트 처리 필요
 
 const Header = () => {
+  const [isSearchModal, setSearchModal] = useState<boolean>(false);
+  const controlSearchModal = (isOpen:boolean) => setSearchModal(isOpen);
     return (
       <div className="header">
           <div className="site-name">
@@ -15,7 +18,8 @@ const Header = () => {
             <Link to='/portfolio' className="tab-name">Portfolio</Link>
             <Link to='/post' className="tab-name">Post</Link>
           </div>
-            <BsSearch className="react-icon" size="40"></BsSearch>
+            <BsSearch className="react-icon" size="40" onClick={e => controlSearchModal(true)}></BsSearch>
+            <Search isOpen={isSearchModal} openSearch={controlSearchModal}></Search>
             <BsPersonCircle className="react-icon" size="40"></BsPersonCircle>
       </div>
     );
